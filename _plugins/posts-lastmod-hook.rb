7674923590d6d7bd33442d567269ed8a8c632523
JunQiang <354888562@qq.com>
2023-03-10 22:37:55 +0800

    Update posts-lastmod-hook.rb

--- _plugins/posts-lastmod-hook.rb
+++ _plugins/posts-lastmod-hook.rb
@@ -21,7 +21,10 @@ Jekyll::Hooks.register :posts, :post_init do |post|
   # Sort commit dates in descending order
   sorted_commit_dates = commit_dates.sort { |a, b| b <=> a }
 
+  # Only keep the latest 10 commit dates
+  latest_commit_dates = sorted_commit_dates.take(10)
+
   # Add sorted commit dates to post metadata
-  post.data['commit_dates'] = sorted_commit_dates
+  post.data['last_modified_at'] = latest_commit_dates
 
 end
