77257271d092f9b0760016a330764fc77ddff3d9
JunQiang <354888562@qq.com>
2023-03-10 22:20:57 +0800

    排序所有的文章用于显示最近更新

--- _plugins/posts-lastmod-hook.rb
+++ _plugins/posts-lastmod-hook.rb
@@ -2,13 +2,26 @@
 #
 # Check for changed posts
 
+# Jekyll::Hooks.register :posts, :post_init do |post|
+
+#   commit_num = `git rev-list --count HEAD "#{ post.path }"`
+
+#   if commit_num.to_i > 1
+#     lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{ post.path }"`
+#     post.data['last_modified_at'] = lastmod_date
+#   end
+
+# end
+
 Jekyll::Hooks.register :posts, :post_init do |post|
 
-  commit_num = `git rev-list --count HEAD "#{ post.path }"`
+  # Get all commit dates for the file
+  commit_dates = `git log --pretty=format:%ad --date=iso "#{post.path}"`.split("\n")
 
-  if commit_num.to_i > 1
-    lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{ post.path }"`
-    post.data['last_modified_at'] = lastmod_date
-  end
+  # Sort commit dates in descending order
+  sorted_commit_dates = commit_dates.sort { |a, b| b <=> a }
+
+  # Add sorted commit dates to post metadata
+  post.data['commit_dates'] = sorted_commit_dates
 
 end
