352fac6380954ad1752bb7b58cadae6ea30fa889
zhujunqiang <junqiang.mail@qq.com>
2024-06-27 12:49:22 +0800

    --check-html

--- local_checker.bat
+++ local_checker.bat
@@ -1,2 +1,3 @@
+@REM bundle exec htmlproofer _site --disable-external --allow-hash_href --trace --ignore-urls "/^http:\/\//" --checks html
 bundle exec htmlproofer _site --disable-external --check-html --allow_hash_href --trace
 pause
